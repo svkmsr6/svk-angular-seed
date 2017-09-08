9e8a157298779dd4a3ea2eb77c0e5eaaffa5d17a
MISRA Souvik <SMA10@dsone.3ds.com>
2017-09-08 14:00:09 +0530

    Karma Changes 2

--- karma.conf.js
+++ karma.conf.js
@@ -22,13 +22,30 @@ module.exports = function(config) {
       'karma-chrome-launcher',
       'karma-firefox-launcher',
       'karma-jasmine',
-      'karma-junit-reporter'
+      'karma-junit-reporter',
+      'karma-html-reporter'
     ],
 
-    junitReporter: {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+    reporters: ['progress', 'html'],
+
+    // the default configuration
+    htmlReporter: {
+      outputDir: 'karma_html', // where to put the reports 
+      templatePath: null, // set if you moved jasmine_template.html
+      focusOnFailures: true, // reports show failures on start
+      namedFiles: false, // name files instead of creating sub-directories
+      pageTitle: null, // page title for reports; browser info by default
+      urlFriendlyName: false, // simply replaces spaces with _ for files/dirs
+      reportName: 'svk-angular-app-report-summary', // report summary filename; browser info by default     
+      // experimental
+      preserveDescribeNesting: false, // folded suites stay folded 
+      foldAll: false, // reports start folded (only with preserveDescribeNesting)
+    },  
+   
+    // junitReporter: {
+    //   outputFile: 'test_out/unit.xml',
+    //   suite: 'unit'
+    // }
 
   });
 };
